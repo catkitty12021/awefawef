22be6785ce9fde5d346c3bfd16d92e142f18cb26
catkitty12021 <80436885+catkitty12021@users.noreply.github.com>
2023-11-22 21:33:13 -0800

    Create cheat2.js

---
+++ cheat2.js
@@ -0,0 +1,63 @@
+// this is a test
+/*
+Points earned from run: 683
+
+Specific results: heads,heads,heads,tails,heads,tails,heads,heads,tails,heads,tails,tails,tails,tails,heads,heads,tails,heads,tails,heads,tails,heads,heads,tails,tails,tails,tails,tails,tails,tails,tails,tails,heads,heads,heads,heads,heads,tails,tails,tails,heads,tails,tails,tails,heads,heads,tails,tails,tails,tails,heads,heads,heads,heads,heads,heads,tails,tails,tails,tails,heads,heads,heads,heads,tails,heads,tails,tails,heads,heads,tails,heads,heads,heads,tails,tails,heads,heads,tails,heads,tails,tails,tails,tails,heads,tails,heads,tails,tails,tails,tails,tails,tails,heads,heads,heads,tails,tails,tails,heads
+
+Verification code: 8307654979, 4734954576
+*/
+
+/*
+a = "170"
+b = "7057"
+c = "47349"
+d = "54576"
+ac = "5757"
+*/
+
+let p = 1e5 + 3
+
+let inv = x => {
+    if (x % p == 0) return 0;
+    for (let i = 1; i < p; i++)
+        if ((x * i) % p == 1) return i;
+};
+
+let e = (f, g) => document.getElementById(f).innerHTML += g;
+// changes the HTML elements
+
+let jsf32 = (a, b, c, d) => {
+        return () => { // returns a function lol
+            a |= 0;
+            b |= 0;
+            c |= 0;
+            d |= 0;
+            // this shouldn't change anything??
+            let t = a - (b << 27 | b >>> 5) | 0;
+            a = b ^ (c << 17 | c >>> 15);
+            b = c + d | 0;
+            c = d + t | 0;
+            d = a + t | 0;
+            return (d >>> 0) / 4294967296;
+        };
+    };
+
+let codes = []
+let arr = [], heads = 0, ac = 0;
+for (let counter = 0; counter < 1000; counter++) {
+    let rngsus = jsf32(48825, 83239, 42678, inv(54576 + counter));
+    arr = []; heads = 0; ac = 0;
+    for (let i = 0; i < 100; i++) {
+        if (rngsus() > 0.5) {
+            arr.push("heads");
+            heads++;
+            ac++;
+        } else arr.push("tails");
+        ac *= 2;
+        ac %= p;
+    }
+    if codes == [] || codes[0] <= Math.floor(1000 * 1.1 ** (heads - 50)) {
+      codes = [String(Math.floor(1000 * 1.1 ** (heads - 50))), String(arr).replace(/heads/g, "1").replace(/tails/g, "0").replace(/,/g, ""), String(ac + p * Math.floor(rngsus() * (1e5 - 3))) + ", " + String(0 + 47349 * 1e5 + 54546 + counter)];
+    }
+}
+e("results", codes);
